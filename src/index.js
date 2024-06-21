95b66166ede100d5cd4a726100b88c78735838ba
zhangleixjy <gdvenus@cecinvestment.com>
2024-06-21 16:00:01 +0800

    mod:修改域名地址为自己的

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.zhang13l.cn": dockerHub,
+  "quay.zhang13l.cn": "https://quay.io",
+  "gcr.zhang13l.cn": "https://gcr.io",
+  "k8s-gcr.zhang13l.cn": "https://k8s.gcr.io",
+  "k8s.zhang13l.cn": "https://registry.k8s.io",
+  "ghcr.zhang13l.cn": "https://ghcr.io",
+  "cloudsmith.zhang13l.cn": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zhang13l.cn": dockerHub,
 };
 
 function routeByHosts(host) {
